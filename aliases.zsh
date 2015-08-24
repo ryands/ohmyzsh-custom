afe02e31e1da15d8aa11346e5762578e6c590a79
ryan <ryan@ryands.org>
2015-08-23 20:42:58 -0700

    alias stuffs

--- aliases.zsh
+++ aliases.zsh
@@ -1,14 +1,32 @@
+# meta
+editzsh() {
+  $EDITOR $HOME/.oh-my-zsh
+}
+
+# git
 alias gpr='git pull -r'
 alias grom='git rebase origin/master'
 alias glog='git log'
 alias wat='git status'
 
+# bundle/ruby/rails
 alias be='bundle exec'
 alias brake='be rake'
 alias brspec='be rspec'
 alias bcap='be cap'
+alias bi='bundle install'
+
+alias rc='rails console'
+alias rs='rails server'
+alias rdb='rails dbconsole'
+alias rdbm='brake db:migrate'
 
 # rorup = Ruby On Rails Update
 rorup() {
   git pull -r && bundle install && bundle exec rake db:migrate
 }
+
+# node stuff
+alias npmi='npm install'
+alias boi='bower install'
+
